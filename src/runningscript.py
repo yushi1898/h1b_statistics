3daf6dbb6c206429943c62e568ff167024b81d31
YS <syhddtsl@gmail.com>
2018-10-27 14:33:46 -0400

    changed directory

--- src/runningscript.py
+++ src/runningscript.py
@@ -1,6 +1,6 @@
 import filesortingfunc as fid
 
-fp = open('../input/h1b_input.csv')
+fp = open('./input/h1b_input.csv')
 
 
 data_state = fid.gencolumn(fp,'WORKSITE_STATE')
@@ -19,7 +19,7 @@ for i in range(len(occupation_sort)):
 state_top_10 = fid.picktop(state_sort,10)
 occupation_top_10 = fid.picktop(occupation_sort,10)
    
-fp_state = open('../output/top_10_states.txt','w')
+fp_state = open('./output/top_10_states.txt','w')
 fp_state.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
 for item in state_top_10:
     fp_state.write("%s;%d;%.1f%%\n" % (item[0],item[1],item[2]*100))
@@ -27,7 +27,7 @@ for item in state_top_10:
 fp_state.close()
 
 
-fp_occupation = open('../output/top_10_occupations.txt','w')
+fp_occupation = open('./output/top_10_occupations.txt','w')
 fp_occupation.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
 for item in occupation_top_10:
     fp_occupation.write("%s;%d;%.1f%%\n" % (item[0],item[1],item[2]*100))
